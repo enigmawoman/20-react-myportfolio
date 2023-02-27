3a38635465ee486e746bbb0e43d33fca652dd321
enigmawoman <112570078+enigmawoman@users.noreply.github.com>
2023-02-27 12:32:58 +0000

    first code add, for the handling of page changes and navigation

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>
+       its all about ME!
+       
+      </p>
+    </div>
+  );
+}
